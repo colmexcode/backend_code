854beef3ebd6b6fbea4d02aa1b6952f957e43b66
orlandCasta <orlandos.casta@gmail.com>
2020-11-13 13:33:57 -0600

    It is added on the get of the top location, the .store is modified with the information to bring, now it brings two posts from Mexico and two posts from Colombia

--- api/components/show/store.js
+++ api/components/show/store.js
@@ -41,18 +41,12 @@ const getLocation = async (location) => {
 //------------------------------------------------------------------------------------------------
 
   const getLocationTop = async () => {
-    //const colombia = await Model.find({ location: {$exists:true}, country: "colombia", image: {$exists:true}}, {location:1, country:1, image:1}).limit(2)
-
-    const mexico1 = await Model.findOne({ _id: "5f9d3ab069fd000017abcd36" }, {location:1, country:1, image:1})
-    const mexico2 = await Model.findOne({ _id: "5f9d3be169fd000017abcd38" }, {location:1, country:1, image:1})
-    const colombia1 = await Model.findOne({ _id: "5f9d3d3a69fd000017abcd39" }, {location:1, country:1, image:1})
-    const colombia2 = await Model.findOne({ _id: "5f9d3d5369fd000017abcd3a" }, {location:1, country:1, image:1})
+    const mexico = await Model.find({ location: {$exists:true}, country: "Mexico", image: {$exists:true}}, {location:1, country:1, image:1}).limit(2)
+    const colombia = await Model.find({ location: {$exists:true}, country: "Colombia", image: {$exists:true}}, {location:1, country:1, image:1}).limit(2)
 
     return  {
-      mexico1,
-      mexico2,
-      colombia1,
-      colombia2
+      mexico,
+      colombia
     }
   }
 
